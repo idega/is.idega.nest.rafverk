39f2c9c6d82cbe9845cb7188a6954f9064c48a77
Valdas Žemaitis <valdas@idega.com>
2015-08-12 13:22:52 +0300

    Using latest DWR

--- src/java/is/idega/nest/rafverk/bean/BaseBean.java
+++ src/java/is/idega/nest/rafverk/bean/BaseBean.java
@@ -9,6 +9,9 @@ import java.util.List;
 
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+import org.directwebremoting.WebContextFactory;
 
 import com.idega.business.IBOLookup;
 import com.idega.business.IBOService;
@@ -28,58 +31,57 @@ public class BaseBean implements Serializable{
 	public static RafverktokuListi getRafverktokuListi(){
 		return (RafverktokuListi) getBean("RafverktokuListi");
 	}
-	
+
 	public static TilkynningLokVerksBean getTilkynningLokVerksBean() {
 		return (TilkynningLokVerksBean) getBean("TilkynningLokVerksBean");
 	}
-	
+
 	public static TilkynningVertakaBean getTilkynningVertakaBean() {
 		return (TilkynningVertakaBean) getBean("TilkynningVertakaBean");
 	}
-	
+
 	public static ChangeElectricianBean getChangeElectricianBean() {
 		return (ChangeElectricianBean) getBean("ChangeElectricianBean");
 	}
-	
+
 	public static User getCurrentUser() {
 		FacesContext context = FacesContext.getCurrentInstance();
 		IWContext iwContext = IWContext.getIWContext(context);
 		return iwContext.getCurrentUser();
 	}
-	
+
 	public static Object getBean(String bean) {
 		FacesContext context = FacesContext.getCurrentInstance();
 		return context.getApplication().getVariableResolver().resolveVariable(context, bean);
 		//ValueBinding binding = context.getApplication().createValueBinding(bean);
 		//return binding.getValue(context);
 	}
-	
+
 	public static InitialData getInitialDataByDWR() {
 		return (InitialData) getBeanByDWR("RafverktakaInitialdata");
-	}	
+	}
 
 	public static RafverktokuListi getRafverktokuListiByDWR(){
 		return (RafverktokuListi) getBeanByDWR("RafverktokuListi");
 	}
-	
+
 	public static TilkynningLokVerksBean getTilkynningLokVerksBeanByDWR() {
 		return (TilkynningLokVerksBean) getBeanByDWR("TilkynningLokVerksBean");
 	}
-	
+
 	public static TilkynningVertakaBean getTilkynningVertakaBeanByDWR() {
 		return (TilkynningVertakaBean) getBeanByDWR("TilkynningVertakaBean");
 	}
-	
+
 	public static ChangeElectricianBean getChangeElectricianBeanByDWR() {
 		return (ChangeElectricianBean) getBeanByDWR("ChangeElectricianBean");
 	}
 
 	public static Object getBeanByDWR(String bean) {
-//		HttpSession session = WebContextFactory.get().getSession();
-//		return session.getAttribute(bean);
-		throw new UnsupportedOperationException();
+		HttpSession session = WebContextFactory.get().getSession();
+		return session.getAttribute(bean);
 	}
-	
+
 	public static Object initializeServiceBean(Object myServiceBeanVariable, Class serviceBeanClass) {
 		if (myServiceBeanVariable != null) {
 			// nothing to do
@@ -87,7 +89,7 @@ public class BaseBean implements Serializable{
 		}
 		return getSeviceBean(serviceBeanClass);
 	}
- 
+
 	public static UIComponent findComponentInRoot(String id) {
 	    FacesContext facesContext = FacesContext.getCurrentInstance();
 	    if (facesContext != null) {
@@ -96,7 +98,7 @@ public class BaseBean implements Serializable{
 	    }
 	    return null;
 	}
-	
+
 	public static UIComponent findComponent(UIComponent component, String id) {
 		List components = new ArrayList();
 		int i = 0;
@@ -115,7 +117,7 @@ public class BaseBean implements Serializable{
 		}
 		while (true);
 	}
-	
+
 	private static IBOService getSeviceBean(Class serviceBeanClass) {
 		IBOService myServiceBean = null;
 		try {
@@ -129,8 +131,8 @@ public class BaseBean implements Serializable{
 		}
 		return myServiceBean;
 	}
-	
-	
+
+
 	public static IWResourceBundle getResourceBundle() {
 		FacesContext facesContext = FacesContext.getCurrentInstance();
 		IWContext iwc = IWContext.getIWContext(facesContext);
